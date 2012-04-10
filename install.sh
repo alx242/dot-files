401ed736457bad05658fe9992f7f1c5c768fccff
Alexander Schüssler <alex@xalx.net>
2012-04-10 22:38:13 +0200

    more n more

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#set -x
+
+DESTDIR="$HOME"
+SRCDIR="$HOME/dot-files"
+SRCITEMS=(
+    bash_profile
+    bashrc
+    inetrc
+    pythonrc
+    emacs
+    emacs.d
+)
+
+mkdir -p $HOME/.config
+
+## First, get all git submodules like .config/awesome/vicious and .emacs.d/distel
+git submodule update --init
+
+for SRCITEM in ${SRCITEMS[*]} ; do 
+    DESTITEM=$(echo $SRCITEM | tr '!' '/')
+    echo "Trying to link $DESTDIR/.$SRCITEM ..."
+    [ -L "$DESTDIR/.$DESTITEM" ] && continue
+    [ -e "$DESTDIR/.$DESTITEM" ] && mv "$DESTDIR/.$DESTITEM" "$DESTDIR/.$DESTITEM.bak"
+    ln -s "$SRCDIR/dot-$SRCITEM" "$DESTDIR/.$DESTITEM"
+done
+
+## Some emacs modules need compilation
+cd $HOME/.emacs.d/distel && make
+cd $HOME
+
+## Initialize some scripts
+source $HOME/.bashrc
