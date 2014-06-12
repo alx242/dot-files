1c64eaf1663efdc7f1bffb4834fd42195bad2254
Alexander Schüssler <alexander.schussler@klarna.com>
2014-06-12 12:03:48 +0200

    Build edts simpler

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ for SRCITEM in ${SRCITEMS[*]} ; do
 done
 
 ## Some emacs modules need compilation
-cd $HOME/.emacs.d/edts && git submodule update --init && make
+cd $HOME/.emacs.d/edts && make
 ## cd $HOME/.emacs.d/distel && git submodule update --init && make
 
 ## Powerline
